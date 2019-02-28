538afb63e6b2e24d365bf509d86f3402dc79fb6c
Ray Weitzenberg <ray@mechaneyes.com>
2019-02-28 17:35:02 -0500

    added GA tagging example

---
+++ gaTagging.js
@@ -0,0 +1,37 @@
+
+
+
+
+onclick='return triggerGA(this)' data-action='Clicked TOC0' data-label='Clicked text link'
+
+
+
+
+let triggerGA = (e) => {
+    event.preventDefault(e)
+   if(e.href) {
+         if(e.href.includes('/men')) {
+         setCk(1, 1000)
+    }
+    else if (e.href.includes('/women')) {
+         setCk(0, 1000)
+    }
+    else if (e.href.includes('/girls') || e.href.includes('/boys')) {
+         setCk(2, 1000)
+    }
+    else if (e.href.includes('/baby')) {
+         setCk(3, 1000)
+    }
+   }
+    dataLayer.push({
+        'event': 'eventTracking',
+        'eventCategory': 'home page',
+        'eventAction': e.dataset.action,
+        'eventLabel': `${e.dataset.label}: ${e.href ? e.href.split('/us/en/')[1] : ''}`,
+        'eventCallback': function() {
+          if(e.href) {
+            location.href=e.href;      	
+          }
+        }, eventTimeout: 1000
+    })
+}
